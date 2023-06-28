cc504355d25032796a24b617c5fb3508afecab15
vikash1012 <vikashbihari10122001@gmail.com>
2023-06-28 12:43:23 +0530

    add project

---
+++ demo/src/test/java/com/sita/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sita.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
