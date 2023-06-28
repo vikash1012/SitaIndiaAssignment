5ba1728d6d791952091118511efa20b6dcd55d18
vikash1012 <vikashbihari10122001@gmail.com>
2023-06-28 21:22:55 +0530

    update project dependency and create mvc

---
+++ backend/src/main/java/com/sitaindia/backend/backend/contoller/EmployeeController.java
@@ -0,0 +1,8 @@
+package com.sitaindia.backend.backend.contoller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+       
+}
---
+++ backend/src/main/java/com/sitaindia/backend/backend/contoller/dto/GetAllEmployeeResponse.java
@@ -0,0 +1,10 @@
+package com.sitaindia.backend.backend.contoller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class GetAllEmployeeResponse {
+    String email; 
+}
