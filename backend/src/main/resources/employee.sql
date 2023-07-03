07b75e9519c4ccf8086f15a9eeafc4e8a0e108b3
vikash1012 <60812660+vikash1012@users.noreply.github.com>
2023-07-03 13:17:45 +0530

    update employee schema.
    this the final schema for the employee table.

--- backend/src/main/resources/employee.sql
+++ backend/src/main/resources/employee.sql
@@ -2,9 +2,12 @@ CREATE TABLE IF NOT EXISTS public.employee
 (
     id integer NOT NULL DEFAULT nextval('employee_id_seq'::regclass),
     amount integer NOT NULL,
-    currency character varying(255) COLLATE pg_catalog."default",
-    emp_name character varying(255) COLLATE pg_catalog."default",
-    exit_date date,
-    joining_date date,
-    dep_id_fk integer NOT NULL DEFAULT nextval('employee_dep_id_fk_seq'::regclass)
+    currency character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    emp_name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    exit_date date NOT NULL,
+    joining_date date NOT NULL,
+    dep_id_fk integer NOT NULL DEFAULT nextval('employee_dep_id_fk_seq'::regclass),
+    CONSTRAINT employee_pkey PRIMARY KEY (id),
+    CONSTRAINT dep_id_fk FOREIGN KEY (dep_id_fk)
+        REFERENCES public.department (id)
 )
